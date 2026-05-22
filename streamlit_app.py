9701e987e1121fdd3f65fb842c1f61b2b4503194
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-22 14:15:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.title("🎈 RaEy apps")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-import streamlit as st
-
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="gray")
 st.header("These headers have rotating dividers", divider=True)
