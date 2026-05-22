45d76c52028980afb355cdb192752c3c18b5ba37
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-22 14:21:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ st.badge("New")
 st.badge("Success", icon=":material/check:", color="green")
 
 st.markdown(
-    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]")
