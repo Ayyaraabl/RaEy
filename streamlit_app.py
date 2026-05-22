46c9f7e5189c2ff83a2b729c914d574ad324cf6d
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-22 14:20:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,9 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.header("_This App_ is :blue[cool] :sunglasses:")
+st.header("_This App_ is :pink[cool] :sunglasses:")
+st.badge("New")
+st.badge("Success", icon=":material/check:", color="green")
+
+st.markdown(
+    ":violet-badge[:material/star: Favorite] :orange-badge[⚠️ Needs review] :gray-badge[Deprecated]"
