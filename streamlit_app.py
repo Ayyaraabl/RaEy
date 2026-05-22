051bcd011167d289b385b490af0367ef708327a9
Ayyaraabl <sarhnblmr@gmail.com>
2026-05-22 14:18:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Hello, this is RaEy app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.header("_This App_ is :blue[cool] :sunglasses:")
